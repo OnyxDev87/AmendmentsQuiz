ec7d037650f7cc23c8809bab1fda4d4bc336d4b3
Miles Wilander <mileswilander@gmail.com>
2025-05-22 13:26:14 -0400

    works better?

--- index.js
+++ index.js
@@ -1,7 +1,10 @@
 var score = 10;
+var question = "What is the 972345th Amendment"
 
 document.querySelector("#score").innerHTML = score;
 
 function restartQuiz() {
     score = 0;
 }
+
+document.querySelector("question").innerHTML = "Question 1: " + question;
