9ee2b3bcb28e30decd05f4bfbfc294c155fa882a
Miles Wilander <mileswilander@gmail.com>
2025-05-22 13:42:39 -0400

    it sorta works now

--- index.js
+++ index.js
@@ -1,10 +1,21 @@
-var score = 10;
+var score = 0;
 var question = "What is the 972345th Amendment"
 
 document.querySelector("#score").innerHTML = score;
 
 function restartQuiz() {
     score = 0;
+    document.querySelector("#score").innerHTML = score;
+    startQuiz();
 }
 
-document.querySelector("question").innerHTML = "Question 1: " + question;
+function startQuiz() {
+    document.querySelector("#question").innerHTML = "Question 1: " + question;
+    document.querySelector("#answer-button1").innerHTML = "there istn't one";
+    document.querySelector("#answer-button1").onclick = function() {
+        score += 1;
+        document.querySelector("#score").innerHTML = score;
+    };
+    document.querySelector("#answer-button2").innerHTML = "incorect";
+    document.querySelector("#answer-button3").innerHTML = "wrong";
+}
